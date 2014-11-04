2b359e298c5683394312ba394c5374690130c2b5
eli <ea2621@columbia.edu>
2014-11-04 09:10:40 -0500

    Some work on AST. Not much. Lots more to do.

--- src/ast.ml
+++ src/ast.ml
@@ -1,11 +1,21 @@
-type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
+type op = Add | Sub | Mult | Div | Mod| Equal | Neq | Less | Leq | Greater | Geq | And | Or | Concat
+          | Graph_Insert | Graph_Remove | Data_Insert | Data_Remove
+
+type uop = Not | Neg
 
 type expr =
-    Literal of int
+    Int of int
+  | Float of float 
+  | String of string 
+  | Bool of bool 
   | Id of string
   | Binop of expr * op * expr
   | Assign of string * expr
   | Call of string * expr list
+  | DataConstruct of  string * expr list
+  | Graph of (* *)
+  | Node of (* *)
+  | Rel of (* *)
   | Noexpr
 
 type stmt =
@@ -22,6 +32,12 @@ type func_decl = {
     body : stmt list;
     return_type : string;
   }
+
+type complex_type =
+  Graph_element of string * expr list
+
+type graph_decl =
+  Graph of complex_type list (* THIS IS NOT DONE/CORRECT *)
 
 type program = string list * func_decl list
 
