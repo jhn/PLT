5895904844ac458c24af125d29cc438be1fa16ff
JIALUN LIU <jl4347@columbia.edu>
2014-11-05 14:35:44 -0500

    Changed if statement and dealing with complex types

--- src/ast.ml
+++ src/ast.ml
@@ -13,27 +13,31 @@ type expr =
   | Assign of expr * expr
   | Call of string * expr list
   | Constructor of  string * expr list
+  | Map of expr * string * statement
   | Graph of (* *)
   | Node of (* *)
   | Rel of (* *)
   | Noexpr
 
-type stmt =
-    Block of stmt list
+type graph_element = 
+   
+
+type statement =
+    Block of statement list (*We called it statement*)
   | Expr of expr
   | Return of expr
-  | If of expr * stmt * stmt
-  | If of expr * stmt (*write both?*)
+  | If of expr * statement * statement
+(*   | If of expr * stmt (*write both?*) *)
 
 
 type func_decl = {
     fname : string;
     formals : string list;
-    body : stmt list;
+    body : statement list;
     return_type : string;
   }
 
-type var = string * var_type;
+type var = var_type * string;
 type var_type = 
     primitive_type
   | complex_type
@@ -45,16 +49,25 @@ type primitive_type =
   | Double
 
 type complex_type =
-  Graph of graph_decl
+  | Graph
+  | Node
+  | Rel
+
+type find_many = 
+  | Find_Many_Pointing_From of string * expr list * graph_element 
+  | Find_Many_Pointing_To of string * string * expr list
+  | Find_Many_Nodes of string * expr list
+
+ (*  | Graph of graph_decl
   | Graph_element of graph_elem_decl
 
 type graph_decl =
   Id of string
   | Graph_element of string * expr list
   
-  (*Graph of complex_type list *) (* THIS IS NOT DONE/CORRECT *)
+  Graph of complex_type list  (* THIS IS NOT DONE/CORRECT *)
 
 type graph_elem_decl = string * expr list
 
-type program = string list * func_decl list
+type program = string list * func_decl list *)
 
