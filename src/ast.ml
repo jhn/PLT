38d111976f01f073378b73203ed325f476d664d2
eli <ea2621@columbia.edu>
2014-11-12 13:59:49 -0500

    reordered and listed whats left to do

--- src/ast.ml
+++ src/ast.ml
@@ -12,9 +12,8 @@ type expr =
   | Double of float 
   | String of string 
   | Bool of bool 
-  |(*Complex literals*)
   | Id of string
-  | Var of var (* ?????? *)
+  | Var of var 
   | Binop of expr * op * expr
   | Unop of uop * expr
   | Assign of expr * expr
@@ -22,14 +21,11 @@ type expr =
   | Call of string * expr list
   | Func of built_in_function_call
   | Constructor of  string * expr list
-  | Graph of graph_type list (*Or expr list? *)
-  | Graph_element of string * expr list (* do this one step removed? *)
-
+  | Graph of graph_type list
+  | Graph_element of string * expr list 
 
 type var = string * n2n_type
-(* Do we need these? *)
-(* type var_declaration = string * type_choices
- *)
+
 type return_ty =
   | Type_spec of type_spec
   | Void
@@ -65,19 +61,15 @@ type map_function =
 type neighbours_function = 
   | Neighbours of string
 
-(*type graph_element_type = 
-   Graph_element of string * expr list*)
 type graph_type = 
   | Graph_type_ID of string
   | Graph_type of string * expr list
 
 type statement =
-    Block of statement list (*We called it statement*)
+    Block of statement list 
   | Expr of expr
   | Return of expr
   | If of expr * statement * statement
-(*   | If of expr * stmt (*write both?*) *)
-
 
 type func_decl = {
     fname : string;
@@ -95,9 +87,6 @@ type program = var list * func_decl list *)
 
 
 (*For semantic check*)
-let string_of_unop = function
-    Neg -> "-"
-  | Not -> "!"
 
 let string_of_binop = function
     Add -> "+" 
@@ -119,42 +108,15 @@ let string_of_binop = function
   | Data_Insert -> "^+"
   | Data_Remove -> "^-"
 
-let string_of_graph_type = function
-  | Graph_type_ID(id) -> string_of_expr id
-  | Graph_type(graph_element) -> string_of_expr graph_element
-
-let string_of_graph_type_list = function
-  | 
-
-let string_of_primitive_type = function
-  | Int -> "Int"
-  | String -> "String"
-  | Bool -> "Bool"
-  | Double -> "Double"
-
-let string_of_complex_type = function
-  | Graph -> "Graph"
-  | Node -> "Node"
-  | Rel -> "Rel"
-
-let string_of_n2n_type = function
-  | N2N_primitive(t) -> string_of_primitive_type t
-  | N2N_complex(t) -> string_of_complex_type t
-
-let string_of_type_spec = function
-  | N2N_type(t) -> string_of_n2n_type t
-  | List(t) -> "List<"^string_of_n2n_type t^">"
-
-let string_of_return_ty = function
-  | Type_spec(t) -> string_of_type_spec t
-  | Void() -> "Void"
+let string_of_unop = function
+    Neg -> "-"
+  | Not -> "!"
 
 let rec string_of_expr = function
     Int(l) -> string_of_int l
   | Double(l) -> string_of_float l
   | String(l) -> "\"" ^ l ^ "\""
   | Bool(l) -> string_of_bool l
-  (* | Null_Literal -> "null" *)
   | Id(s) -> s
   | Var(v) ->
       (match (fst v) with
@@ -173,8 +135,56 @@ let rec string_of_expr = function
   | Access(id1, id2) -> string_of_expr id1 ^ "." ^ string_of_expr id2
   | Call(f, el) ->
       string_of_expr f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Func (*Implement this*)
+  | Constructor (*Implement this*)
   | Graph(graph_type_l) ->
       "("^ String.concat "," List.map string_of_graph_type graph_type_l  ^ ")"
        (*Modification needed*)
   | Graph_element(id, el) ->
       string_of_expr id ^ "[" ^ String.concat ", " (List.map string_of_expr el) ^ "]"
+
+let string_of_return_ty = function
+  | Type_spec(t) -> string_of_type_spec t
+  | Void() -> "Void"
+
+let string_of_type_spec = function
+  | N2N_type(t) -> string_of_n2n_type t
+  | List(t) -> "List<"^string_of_n2n_type t^">"
+
+let string_of_n2n_type = function
+  | N2N_primitive(t) -> string_of_primitive_type t
+  | N2N_complex(t) -> string_of_complex_type t
+
+let string_of_primitive_type = function
+  | Int -> "Int"
+  | String -> "String"
+  | Bool -> "Bool"
+  | Double -> "Double"
+
+let string_of_complex_type = function
+  | Graph -> "Graph"
+  | Node -> "Node"
+  | Rel -> "Rel"
+
+  (* BUILT IN FUNCTION CALL *)
+  (* MAP *)
+  (* NEIGHBORS *)
+
+let string_of_graph_type = function
+  | Graph_type_ID(id) -> string_of_expr id
+  | Graph_type(graph_element) -> string_of_expr graph_element
+
+  (* FUNC_DECL *)
+  (* FIND MANY *)
+   
+
+
+
+
+
+
+
+
+
+
+
