afcb4cab950f54aed3d3c737c08ef9b5ec4a9c4d
eli <ea2621@columbia.edu>
2014-12-03 13:51:50 -0500

    sast and ast in sync aside from var decl

--- src/sast.mli
+++ src/sast.mli
@@ -4,19 +4,19 @@ type var_type =
   Int
 | Double
 | String
-| Boolean
+| Bool
 | Void
 | Rel
 | Node
 | Graph
 | List of var_type
 
+type sformal = 
+  Formal of var_type * string
+
 type sExpr =
-    SInt of int * var_type
-  | SDouble of float * var_type
-  | SString of string * var_type
-  | SBool of bool * var_type
-  | SId of string * var_type (*??Look at Livi's*)
+    SLiteral of sliteral * var_type
+  | SId of string * var_type
   | SBinop of sExpr * Ast.op * sExpr * var_type
   | SUnop of Ast.uop * sExpr * var_type
   | SAssign of sExpr * sExpr * var_type
@@ -24,7 +24,12 @@ type sExpr =
   | SCall of string * sExpr list * var_type
   | SFunc of sbuilt_in_function_call * var_type
   | SComplex of scomplex_literal * var_type
-  | SVar of svar_decl * var_type
+
+type sliteral =
+  SInt_Literal of int
+  | SDouble_Literal of float
+  | SString_Literal of string
+  | SBool_Literal of bool
 
 type sbuilt_in_function_call =
   SFindMany of string * sExpr * sExpr option
@@ -32,12 +37,8 @@ type sbuilt_in_function_call =
   | SNeighbors_Func of string * string
 
 type scomplex_literal =
-  | SGraph of sNode_rel_Node_tuple list
-  | SGraph_element of string * sExpr list
-
-type svar_decl =
-  SVar of string * sExpr
-  | SVarDecLiteral of string * sExpr * scomplex_literal
+  | SGraph_Literal of sNode_rel_Node_tuple list
+  | SGraph_Element of string * sExpr list
 
 type sNode_rel_Node_tuple =
   SNode_Rel_Node_tup of sgraph_type * sgraph_type * sgraph_type
@@ -52,20 +53,20 @@ type sstatement =
   | SReturn of sExpr
   | SIf of sExpr * sstatement * sstatement
 
+type svar_decl =
+  SVar of string * sExpr
+  | Constructor of string * sExpr * sformal list
+  | SVarDecLiteral of string * sExpr * scomplex_literal
+
 type sfunc_type = {
-  sfname : var_type;
-  sformals : string list;
+  sfname : string;
+  sformals : sformal list;
   sbody : sstatement list;
   sreturn_type : var_type;
-  }
+}
 
 type sfunc_decl =
   SFunc_Decl of sfunc_type * var_type
 
 type sprogram =
   Prog of svar_decl list * sfunc_decl list
-
-
-
-
-
