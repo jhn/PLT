3f44ddc1318893f1c5689fdb7943133d2e590872
eli <ea2621@columbia.edu>
2014-12-12 15:37:26 -0500

    Expressions, statements, var and func declarations

--- src/code_gen.ml
+++ src/code_gen.ml
@@ -39,14 +39,21 @@ let translate (globals, functions) =
 
   (* Turns a literal object into a java expr. TODO: More literals *)
   in let rec string_of_literal = function
-    | StringLiteral(s)  -> "\"" ^ s ^ "\""
-    | BooleanLiteral(s) -> string_of_bool s
+    | String_Literal(s)  -> "\"" ^ s ^ "\""
+    | Boolean_Literal(s) -> string_of_bool s
+    | Int_Literal(s) -> string_of_int s
+    | Double_Literal(s) -> string_of_float s
 
     in let gen_id = function
     | Id(id) -> id
 
     in let gen_formal = function
     | Formal(type_spec,id) -> gen_var_type type_spec gen_id id
+
+	in let gen_formal_list fl = match fl with
+	[] -> ""
+	| head::[] -> gen_formal head
+	| head::tail -> gen_formal head ^ ", " ^ gen_formal_list tail
 
     in let gen_binop = function
     | Add     -> "+"
@@ -68,10 +75,9 @@ let translate (globals, functions) =
     | Not -> "!"
     | Neg -> "-"
 
-
-     in let gen_graph_op = function
-     | Graph_Insert ->
-     | Graph_Remove ->
+    in let gen_graph_op = function
+    | Graph_Insert -> 
+    | Graph_Remove ->
 
 let gen_expr expr = match expr with
 Literal(l) -> (match l with
@@ -90,12 +96,45 @@ Literal(l) -> (match l with
 				else gen_id id ^ "(" ^ gen_expr_list e1 ^ ");"
 | Func(fname) -> match fname with
 	| Find_Many(id,e1) -> gen_id id ^ ".findMany(" ^ get_expr_list e1 ^ ");"
-	|
+	| Map(id,e1) -> gen_id id ^ ".map(" ^ get_stmt_list ^ ");"
+	| Neighbors_Func(id,e1) -> gen_id id ^ ".neighbors(" ^ get_stmt_list ^ ");"
 
-let gen_expr_list expr_list = match expr_list with
+and gen_expr_list expr_list = match expr_list with
 [] -> ""
 | head::[] -> gen_expr head
 | head::tail -> gen_expr head ^ ", " ^ gen_expr_list tail
+
+and gen_stmt stmt = match stmt with 
+Block(stmt_list) -> "{\n\t" ^ gen_stmt_list stmt_list ^ "\n}\n"
+| Expr(expr) -> gen_expr expr ^ ";\n"
+| Return(expr) -> "return " ^ gen_expr expr ^ ";\n\t"
+| If(expr,s1,s2) -> if(s2) "if(" ^ gen_expr expr ^ ") {\n\t" ^ gen_stmt s1 ^ 
+	^ "\n\telse " ^ gen_stmt s2
+| Var_Decl(vdec) -> gen_var_dec vdec ^";\n\t"
+
+and gen_stmt_list stmt_list = match stmt_list with
+[] -> ""
+| head::[] -> gen_stmt head
+| head::tail -> gen_stmt head ^ gen_stmt_list tail
+
+and gen_var_dec dec = match dec with
+Var(ty,id) -> string_of_data_type ty ^ " " ^ gen_id id ^ ";"
+| Var_Primitive_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = " ^ gen_expr ex ^ ";"
+| Var_Complex_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = new " ^ string_of_data_type ty ^ "();" 
+(* What goes in parentheses is based on how java classes are set up. #Jhn *)
+(* May have to split up between graphs, nodes, and rels in a new function *)
+| Access_Assign(e1, e2) ->  
+| Constructor(comp,id,formals) -> 
+
+(* Static? Need if being used in main method right? *)
+and gen_func_dec func =
+"public static " ^ func.return_type ^ " " ^ gen_id func.fname ^
+"(" ^ gen_formal_list ^ func.formals ^ ") {\n" ^ gen_stmt_list func.body ^ "}\n"
+
+and gen_func_dec_list fl =
+[] -> ""
+| head::[] -> gen_func_dec head
+| head::tail -> gen_func head ^ gen_func_dec_list tail
 
 let gen_graph_elem_op = function
 Data_Insert ->
@@ -111,15 +150,4 @@ let prog_gen = function
 	gen_funcs checked_functions ^
 	"public static void main(String[] args){\n\t" ^
 	gen_main_method main^ "\n}\n}"
-
-
-class declaration
-method declarations
-main method 
-
- public class LeastSquares{
-
-    public static String[] readFile(String fileName){
-      public static void main(String[] args) { 
-
 
