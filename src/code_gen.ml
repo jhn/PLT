bfcac0f7913c8052321e2fd0c08f68182a8ca002
eli <ea2621@columbia.edu>
2014-12-13 21:44:23 -0500

    Added Sast functions and fixing expressions

--- src/code_gen.ml
+++ src/code_gen.ml
@@ -24,7 +24,7 @@ let rec get_type_name = function
 
 (* Returns the java declaration for a type. Takes care of formals and actuals. *)
 let rec string_of_data_type with_id = function
-  | Int(id)    -> "int " ^ (if with_id then id else "")
+  | Int    -> "int " ^ (if with_id then id else "")
   | Double(id) -> "double " ^ (if with_id then id else "")
   | String(id) -> "String " ^ (if with_id then id else "")
   | Bool(id)   -> "boolean " ^ (if with_id then id else "")
@@ -35,15 +35,12 @@ let rec string_of_data_type with_id = function
   | Graph(id)  -> "Graph " ^ (if with_id then id else "")
   | Void(id)   -> "void " ^ (if with_id then id else "")
 
-(* Turns a literal object into a java expr. TODO: More literals *)
+(*Contained within gen_expr. May not be needed.*)
 in let rec string_of_literal = function
-  | String_Literal(s)  -> "\"" ^ s ^ "\""
+  | SString_Literal(s)  -> "\"" ^ s ^ "\""
   | Boolean_Literal(s) -> string_of_bool s
   | Int_Literal(s)     -> string_of_int s
   | Double_Literal(s)  -> string_of_float s
-
-let gen_id = function
-  | Id(id) -> id
 
 let gen_formal = function
   | Formal(type_spec, id) -> string_of_data_type type_spec true
@@ -53,14 +50,14 @@ let gen_formal_list fl = match fl with
   | head::[] -> gen_formal head
   | head::tail -> gen_formal head ^ ", " ^ gen_formal_list tail
 
-let gen_unop = function
-  | Not -> "!"
-  | Neg -> "-"
-
-(* TODO: this probably should be another category for builtin functions *)
-let gen_graph_op = function
+(* TODO: These dont currently exist in java backend *)
+let gen_graph_op grop = match grop with 
   | Graph_Insert -> ".insert("
   | Graph_Remove -> ".remove("
+
+ let gen_graph_elem_op geop = match geop with
+  | Data_Insert -> ".insert("
+  | Data_Remove -> ".remove("
 
 let gen_binop = function
   | Add     -> "+"
@@ -78,35 +75,49 @@ let gen_binop = function
   | Or      -> "||"
   | Concat  -> "+="
 
-let gen_expr expr = match expr with
-  | Literal(l) -> (match l with
-    | Int_Literal(i) -> string_of_int i
-    | Double_Literal(d) -> string_of_float d
-    | Bool_Literal(b) -> string_of_bool b
-    | String_Literal(str) -> "\"" ^ str ^ "\"" )
-  | Complex(c) -> match c with
-      Graph_Literal(nrn_list) -> 
-  | Graph_Element(id, lit_list) ->
-  | Id(v) -> gen_id v
-  | Unop(u, e) -> gen_unop u ^ "(" ^ gen_expr e ^ ")"
-  | Binop(e1, op, e2) -> gen_expr e1 ^ gen_binop op ^ gen_expr e2
-  | Access(e1,e2) -> gen_expr e1 ^ "." ^ gen_expr
-  | Call(id, e1) -> gen_id id ^ "(" ^ gen_expr_list e1 ^ ");"
-  | Func(fname) -> match fname with
-    | Find_Many(id, e1) -> gen_id id ^ ".findMany(" ^ get_expr_list e1 ^ ");"
-    | Map(id, e1) -> gen_id id ^ ".map(" ^ get_stmt_list ^ ");"
-    | Neighbors_Func(id, e1) -> gen_id id ^ ".neighbors(" ^ get_stmt_list ^ ");"
+ let gen_unop = function
+  | Not -> "!"
+  | Neg -> "-"
+
+let rec gen_expr expr = match expr with
+  | SLiteral(l,t) -> (match l with
+    | SInt_Literal(i) -> string_of_int i
+    | SDouble_Literal(d) -> string_of_float d
+    | SBool_Literal(b) -> string_of_bool b
+    | SString_Literal(str) -> "\"" ^ str ^ "\"" )
+  | SId(v,t) -> v
+  | SComplex(c,t) -> (match c with
+  	| SGraph_Literal(nrn_list) -> gen_node_rel_node_tup nrn_list (* Should just be a list of graph elements *)
+	| SGraph_Element(id, lit_list) -> id ^ "(" ^ gen_expr_list lit_list ^ ");" )
+  | SUnop(u, e, t) -> gen_unop u ^ "(" ^ gen_expr e ^ ")"
+  | SBinop(e1, op, e2, t) -> gen_expr e1 ^ gen_binop op ^ gen_expr e2
+  | SAccess(e1,e2, t) -> e1 ^ "." ^ e2
+  | SCall(id, e1, t) -> id ^ "(" ^ gen_expr_list e1 ^ ");"
+  | SFunc(fname, t) -> (match fname with
+  	(* TOASK How call Map and Neighbors function? And Graph/Data inserts *)
+    | SFind_Many(id, e1, e2) -> gen_id id ^ ".findMany(" ^ get_expr_list e1 ^ ");"
+    | SMap(e1, e2, id, s1) -> gen_id id ^ ".map(" ^ get_stmt_list ^ ");"
+    | SNeighbors_Func(id, id) -> gen_id id ^ ".neighbors(" ^ get_stmt_list ^ ");" )
+  | SGrop(e1, grop, e2, t) ->
+  | SGeop(e1, geop, e2, t) -> gen_expr e1 ^ 
 
 and gen_expr_list expr_list = match expr_list with
   [] -> ""
   | head::[] -> gen_expr head
   | head::tail -> gen_expr head ^ ", " ^ gen_expr_list tail
 
+and gen_node_rel_node_tup nrn_list = function 
+  SNode_Rel_Node_tup(sg1, sg2, sg3) -> gen_sgraph_type sg1 ^ ", " ^ gen_sgraph_type sg2 ^ ", " ^ gen_sgraph_type sg3
+
+and gen_sgraph_type gt = match gt with 
+  | SGraph_Id(id) -> id
+  | SGraph_type(e1) -> gen_expr e1
+
 and gen_stmt stmt = match stmt with
   Block(stmt_list) -> "{\n\t" ^ gen_stmt_list stmt_list ^ "\n}\n"
   | Expr(expr) -> gen_expr expr ^ ";\n"
   | Return(expr) -> "return " ^ gen_expr expr ^ ";\n\t"
-  | If(expr,s1,s2) -> if(s2) "if(" ^ gen_expr expr ^ ") {\n\t" ^ gen_stmt s1 ^
+  | If(expr,s1,s2) -> "if(" ^ gen_expr expr ^ ") {\n\t" ^ gen_stmt s1 ^
     "\n\telse " ^ gen_stmt s2
   | Var_Decl(vdec) -> gen_var_dec vdec ^";\n\t"
 
@@ -124,7 +135,6 @@ and gen_var_dec dec = match dec with
   | Access_Assign(e1, e2) ->
   | Constructor(comp,id,formals) ->
 
-(* Static? Need if being used in main method right? *)
 and gen_func_dec func =
 "public static " ^ func.return_type ^ " " ^ gen_id func.fname ^
 "(" ^ gen_formal_list ^ func.formals ^ ") {\n" ^ gen_stmt_list func.body ^ "}\n"
@@ -133,13 +143,6 @@ and gen_func_dec_list fl =
   | [] -> ""
   | head::[] -> gen_func_dec head
   | head::tail -> gen_func head ^ gen_func_dec_list tail
-
-let gen_graph_elem_op = function
-  | Data_Insert ->
-  | Data_Remove ->
-
-let gen_main_method = function
-  Main() ->
 
 let prog_gen = function
   Prog(checked_globals, checked_functions) ->
