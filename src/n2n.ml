a09d3bb65d30dcd31928b93ed8783474939406fa
Johan Mena <jmm2371@columbia.edu>
2014-12-14 17:04:30 -0500

    Create the n2n binary to handle different actions

---
+++ src/n2n.ml
@@ -0,0 +1,38 @@
+open Unix
+
+type action = Ast | Symtab | SAnalysis | Compile | Binary | Help
+
+let usage (name:string) =
+  "usage:\n" ^ name ^ "\n" ^
+    "        -a source.ml              (Print AST of source)\n"
+
+let _ =
+  let action = 
+  if Array.length Sys.argv > 1 then
+    (match Sys.argv.(1) with
+        "-a" -> if Array.length Sys.argv == 3 then Ast else Help
+      | "-t" -> if Array.length Sys.argv == 3 then Symtab else Help
+      | "-s" -> if Array.length Sys.argv == 3 then SAnalysis else Help
+      | "-c" -> if (Array.length Sys.argv == 3) || (Array.length Sys.argv == 4) then Compile else Help
+      | "-b" -> if (Array.length Sys.argv == 3) || (Array.length Sys.argv == 4) then Binary else Help
+      | _ -> Help)
+  else Help in   
+
+  match action with
+      Help -> print_endline (usage Sys.argv.(0)) 
+    | (Ast | Symtab | SAnalysis | Compile | Binary ) ->
+      let input = open_in Sys.argv.(2) in
+      let lexbuf = Lexing.from_channel input in
+      let program = Parser.program Scanner.token lexbuf in
+      (match action with
+          Ast -> let listing = Ast.string_of_program program
+                 in print_string listing
+        | Symtab -> let listing = Ast.string_of_program program
+                 in print_string listing
+        | SAnalysis -> let listing = Ast.string_of_program program
+                 in print_string listing
+        | Compile -> let listing = Ast.string_of_program program
+                 in print_string listing
+        | Binary ->  let listing = Ast.string_of_program program
+                 in print_string listing
+        | Help -> print_endline (usage Sys.argv.(0))) (* impossible case *)
