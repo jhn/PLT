9b16b4e4ec056a42c18b82f56b8624416fb7d9ec
eli <ea2621@columbia.edu>
2014-12-13 23:13:54 -0500

    Finished Matching against SAST

--- src/code_gen.ml
+++ src/code_gen.ml
@@ -9,6 +9,17 @@ let imports =
   "import com.n2n.Node;\n" ^
   "import com.n2n.Relationship;\n"
 
+ let gen_var_type = function
+ 	Int -> "int"
+  | Double -> "double"
+  | Bool -> "boolean"
+  | String -> "String"
+  | Void -> "void"
+  | Rel -> "Relationship"
+  | Node -> "Node"
+  | Graph -> "Graph"
+  | List -> "List"
+
 in let rec string_of_literal = function
   | SString_Literal(s)  -> "\"" ^ s ^ "\""
   | Boolean_Literal(s) -> string_of_bool s
@@ -59,9 +70,9 @@ let rec gen_expr expr = match expr with
   | SCall(id, e1, t) -> id ^ "(" ^ gen_expr_list e1 ^ ");"
   | SFunc(fname, t) -> (match fname with
   	(* TOASK How call Map and Neighbors function? And Graph/Data inserts *)
-    | SFind_Many(id, e1, e2) -> gen_id id ^ ".findMany(" ^ get_expr_list e1 ^ ");"
-    | SMap(e1, e2, id, s1) -> gen_id id ^ ".map(" ^ get_stmt_list ^ ");"
-    | SNeighbors_Func(id, id) -> gen_id id ^ ".neighbors(" ^ get_stmt_list ^ ");" )
+    | SFind_Many(id, sfm) -> id ^ ".findMany(" ^ gen_find_many sfm ^ ");"
+    | SMap(id, smf) -> gen_id id ^ ".map(" ^ gen_map smf ^ ");"
+    | SNeighbors_Func(id1, id2) -> id1 ^ ".neighbors(" ^ id2 ^ ");" )
   | SGrop(e1, grop, nrn, t) -> gen_expr e1 ^ gen_graph_op grop ^ gen_node_rel_node_tup nrn ^ ");"
   | SGeop(e1, geop, e2, t) -> gen_expr e1 ^ gen_graph_elem_op geop ^ gen_expr e2 ^ ");"
 
@@ -69,6 +80,14 @@ and gen_expr_list expr_list = match expr_list with
   [] -> ""
   | head::[] -> gen_expr head
   | head::tail -> gen_expr head ^ ", " ^ gen_expr_list tail
+
+and gen_find_many sfind = match sfind with
+  | SFind_Many_Node(scomp) -> gen_expr scomp
+  | SFind_Many_Gen(gt1, gt2) -> gen_sgraph_type gt1 ^ ", " ^ gen_sgraph_type gt2
+
+(* TODO: Figure out what to pass into the Map function when created in Javac Backedn *)
+and gen_map smap = function
+  SMap_Func(id,sl) -> 
 
 (* TODO: These dont currently exist in java backend *)
 and gen_graph_op grop = match grop with 
@@ -86,27 +105,31 @@ and gen_sgraph_type gt = match gt with
   | SGraph_Id(id) -> id
   | SGraph_type(e1) -> gen_expr e1
 
-and gen_stmt stmt = match stmt with
-  Block(stmt_list) -> "{\n\t" ^ gen_stmt_list stmt_list ^ "\n}\n"
-  | Expr(expr) -> gen_expr expr ^ ";\n"
-  | Return(expr) -> "return " ^ gen_expr expr ^ ";\n\t"
-  | If(expr,s1,s2) -> "if(" ^ gen_expr expr ^ ") {\n\t" ^ gen_stmt s1 ^
-    "\n\telse " ^ gen_stmt s2
-  | Var_Decl(vdec) -> gen_var_dec vdec ^";\n\t"
+and gen_sstmt stmt = match stmt with
+  SBlock(stmt_list) -> "{\n\t" ^ gen_sstmt_list stmt_list ^ "\n}\n"
+  | SExpr(expr) -> gen_expr expr ^ ";\n"
+  | SReturn(expr) -> "return " ^ gen_expr expr ^ ";\n\t"
+  | SIf(expr,s1,s2) -> "if(" ^ gen_expr expr ^ ") {\n\t" ^ gen_stmt s1 ^
+    "\n\telse " ^ gen_sstmt s2
+  | SVar_Decl(vdec) -> gen_var_dec vdec ^";\n\t"
 
-and gen_stmt_list stmt_list = match stmt_list with
+and gen_sstmt_list stmt_list = match stmt_list with
   | [] -> ""
   | head::[] -> gen_stmt head
   | head::tail -> gen_stmt head ^ gen_stmt_list tail
 
 and gen_var_dec dec = match dec with
-  Var(ty,id) -> string_of_data_type ty ^ " " ^ gen_id id ^ ";"
-  | Var_Primitive_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = " ^ gen_expr ex ^ ";"
-  | Var_Complex_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = new " ^ string_of_data_type ty ^ "();"
+  	SVar(ty,id) -> gen_var_type ty ^ " " ^ gen_id id ^ ";"
+ (* | Var_Primitive_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = " ^ gen_expr ex ^ ";" *)
+ (* | Var_Complex_Decl_Assign(id, ty, ex) -> string_of_data_type ty ^ " " ^ gen_id id ^ " = new " ^ string_of_data_type ty ^ "();" *)
 (* What goes in parentheses is based on how java classes are set up. #Jhn *)
 (* May have to split up between graphs, nodes, and rels in a new function *)
-  | Access_Assign(e1, e2) ->
-  | Constructor(comp,id,formals) ->
+  | SConstructor(ty,id,formals) -> gen_var_type ty ^ " " ^ id ^ " = new " ^ gen_var_type ty ^ "(" ^ gen_formal_list formals ^ ");"
+  | SVar_Decl_Assign(id,ty,e) -> (match ty with 
+  	 Int | Double | Bool | String -> gen_var_type ty ^ " " ^ id ^ " = " ^ gen_expr e ^ ";"
+   | Rel | Node | Graph | List -> gen_var_type ty ^ " " ^ id ^ " = new " ^ gen_var_type ty ^ "(" ^ gen_expr e ^ ");" )
+  | SAccess_Assign(e1, e2) -> gen_expr e1 ^ " = " gen_expr e2 
+
 
 and gen_func_dec func =
 "public static " ^ func.return_type ^ " " ^ gen_id func.fname ^
