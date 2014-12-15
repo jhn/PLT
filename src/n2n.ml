08162bf9f5f0c1fa55df535bdb1f3c8e90a40375
JIALUN LIU <jl4347@columbia.edu>
2014-12-15 17:41:26 -0500

    Added flow for compiling, still need to add code_gen in the Makefile

--- src/n2n.ml
+++ src/n2n.ml
@@ -1,38 +1,37 @@
 open Unix
 
-type action = Ast | Symtab | SAnalysis | Compile | Binary | Help
+type action = Ast | SAnalysis | Compile | Help
 
 let usage (name:string) =
   "usage:\n" ^ name ^ "\n" ^
-    "        -a source.ml              (Print AST of source)\n"
+    "        -a source.n2n                 (Print AST of source)\n"^
+    "        -s source.n2n                 (Run Semantic Analysis over source)\n"^
+    "        -c source.n2n [target.java]   (Compile to java. Second argument optional)\n"
 
 let _ =
   let action = 
   if Array.length Sys.argv > 1 then
     (match Sys.argv.(1) with
         "-a" -> if Array.length Sys.argv == 3 then Ast else Help
-      | "-t" -> if Array.length Sys.argv == 3 then Symtab else Help
       | "-s" -> if Array.length Sys.argv == 3 then SAnalysis else Help
       | "-c" -> if (Array.length Sys.argv == 3) || (Array.length Sys.argv == 4) then Compile else Help
-      | "-b" -> if (Array.length Sys.argv == 3) || (Array.length Sys.argv == 4) then Binary else Help
       | _ -> Help)
   else Help in   
 
   match action with
       Help -> print_endline (usage Sys.argv.(0)) 
-    | (Ast | Symtab | SAnalysis | Compile | Binary ) ->
+    | (Ast | SAnalysis | Compile ) ->
       let input = open_in Sys.argv.(2) in
       let lexbuf = Lexing.from_channel input in
       let program = Parser.program Scanner.token lexbuf in
       (match action with
           Ast -> let listing = Ast.string_of_program program
                  in print_string listing
-        | Symtab -> let listing = Ast.string_of_program program
-                 in print_string listing
         | SAnalysis -> let env = Semantic_check.run_program program in
-                    ignore env; print_string "Passed Semantic Analysis.\n"
-        | Compile -> let listing = Ast.string_of_program program
-                 in print_string listing
-        | Binary ->  let listing = Ast.string_of_program program
-                 in print_string listing
+                      ignore env; print_string "Passed Semantic Analysis.\n"
+        | Compile -> let sast = Semantic_check.run_program program in
+                      ignore sast; print_string "Passed Semantic Analysis.\n"
+                     (* let compiled_program = Code_gen.pro_gen sast in
+                     if Array.length Sys.argv == 3 then print_endline compiled_program
+                     else let out = open_out Sys.argv.(3) in output_string out compiled_program; close_out out *)
         | Help -> print_endline (usage Sys.argv.(0))) (* impossible case *)
