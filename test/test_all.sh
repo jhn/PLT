00047d415c92dddb28b73b314f922d0ace040e8f
Johan Mena <jmena@pulsepoint.com>
2014-12-16 17:49:07 -0500

    Create stupid primitive script for testing java output

---
+++ test/test_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COMPILER="../src/n2n -c"
+TESTFILES=$@
+
+for f in $TESTFILES
+do
+	LEN=$((${#f}-4))
+	OUTFILENAME="${f:0:$LEN}.output"
+	TESTFILENAME="${f:0:$LEN}.out"
+  echo -n "Test $f: "
+	$COMPILER $f > $OUTFILENAME 2>> errors.out
+
+  DIFF=$(diff -w $OUTFILENAME $TESTFILENAME)
+  if [ "$DIFF" == "" ]
+	then
+		echo " OK"
+	else
+		echo " FAIL"
+    echo "$DIFF"
+	fi
+
+  rm -f $OUTFILENAME
+done
