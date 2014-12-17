ee0a3cb0c6d5575476e4300517d835a507489a31
Johan Mena <jmm2371@columbia.edu>
2014-12-16 23:37:54 -0500

    add friendlier output for Graphs

--- backend/src/com/n2n/Graph.java
+++ backend/src/com/n2n/Graph.java
@@ -166,4 +166,10 @@ public class Graph {
         addToGraph(relatedMemberList);
     }
 
+    @Override
+    public String toString() {
+        return "Graph{" +
+                "relationships=" + relationships +
+                '}';
+    }
 }
