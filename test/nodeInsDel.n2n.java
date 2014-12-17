58a1f5f07cd4bb8abd809b40383523e63eda279d
JIALUN LIU <jl4347@columbia.edu>
2014-12-17 14:00:44 -0500

    Test program changes

---
+++ test/nodeInsDel.n2n.java
@@ -0,0 +1,27 @@
+package com.n2n;
+
+import java.util.*;
+
+class Main {
+
+	static String actor = "actor";
+;public static void main(String[] args) {
+
+		Node Keanu = new Node("actor", new HashMap<String, Object>() {{
+	put("name", "Keanu");
+put("age", 35);
+
+}}
+);
+	System.out.print(Keanu);
+	System.out.print("\n");
+	Keanu.insert(boolean visited);
+	Keanu.getData().put("visited", true);
+	System.out.print(Keanu);
+	System.out.print("\n");
+	Keanu.remove(boolean visited);
+	System.out.print(Keanu);
+	System.out.print("\n");
+	}
+
+}
